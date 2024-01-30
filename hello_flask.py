1474b008501acf2128364d8a67b1ca5f4f83e830
unknown <srinithin>
2024-01-30 17:15:31 +0530

    Added pageone and pagetwo

--- hello_flask.py
+++ hello_flask.py
@@ -6,5 +6,11 @@ def index():
 @app.route("/login")
 def template():
     return render_template("index.html")
+@app.route("/pageone")
+def pageone():
+    return render_template("firstpage.html")
+@app.route("/pagetwo")
+def pagetwo():
+    return render_template("secondpage.html")
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
