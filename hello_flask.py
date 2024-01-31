0c1b3cfa057ed9a981223f796168db3c4407b37b
unknown <srinithin>
2024-01-31 11:28:12 +0530

    Inner Redirect-(frontpg,resultpg,printtime)

--- hello_flask.py
+++ hello_flask.py
@@ -1,4 +1,5 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,redirect
+import datetime
 app = Flask(__name__)
 @app.route("/")
 def index():
@@ -12,5 +13,17 @@ def pageone():
 @app.route("/pagetwo")
 def pagetwo():
     return render_template("secondpage.html")
+@app.route("/frontpage")
+def frontpage():
+    return render_template("frontpage.html")
+@app.route("/resultpage")
+def resultpage():
+    return render_template("resultpage.html")
+@app.route("/printtime")
+def printtime():
+    print()
+    print(datetime.datetime.now())
+    print()
+    return redirect("/resultpage")
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
