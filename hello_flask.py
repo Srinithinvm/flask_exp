b36ddcad5a72df3ff0509737f22aee19190b39e6
unknown <srinithin>
2024-02-01 17:10:23 +0530

    SQlite table show data

--- hello_flask.py
+++ hello_flask.py
@@ -1,7 +1,9 @@
 from flask import Flask,render_template,redirect,request
-import datetime
+import datetime,sqlite3
 app = Flask(__name__)
 
+insertQuery='INSERT INTO datas(name,password,gender) VALUES("%s","%s","%s")'
+fetchQuery='SELECT * from datas'
 
 @app.route("/")
 def index():
@@ -54,6 +56,7 @@ def inputpage():
 @app.route("/statuspage",methods=["post"])
 def statuspage():
     status=request.form.get("textinput")
+   
     return render_template("statuspage.html",status=status)
     
 @app.route("/templateworks")
@@ -62,6 +65,50 @@ def templateworks():
     return render_template("templateworks.html",data = pythondata)
     
 
-   
+@app.route("/testpage")
+def testpage():
+    return render_template("testpage.html")
+    
+@app.route("/testpage2",methods=["post"])
+def testpage2():
+    name=request.form.get("textinput1")
+    password=request.form.get("textinput2")
+    gender=request.form.get("textinput3")
+    con=sqlite3.connect("database.db")
+    c=con.cursor()
+    c.execute(insertQuery%(name,password,gender))
+    con.commit()
+    con.close()
+    return render_template("testpage2.html",name=name,password=password,gender=gender)
+
+  
+def Create_table():
+    createQuery="""
+                    CREATE TABLE IF NOT EXISTS datas(
+                        pid INTEGER PRIMARY KEY,
+                        Name TEXT  NOT NULL,
+                        Password TEXT NOT NULL,
+                        Gender TEXT NOT NULL
+                        )
+                    """
+    con=sqlite3.connect("database.db")
+    c=con.cursor()
+    c.execute(createQuery)
+    con.commit()
+    con.close()
+@app.route("/view")
+def Fetch_all():
+    con=sqlite3.connect("database.db")
+    c=con.cursor()
+    c.execute(fetchQuery)
+    receivedData=c.fetchall()
+    return render_template("view.html",results=receivedData)
+
+        
+
+       
 if __name__ == "__main__":
+
+    Create_table()
+    
     app.run(host="0.0.0.0",debug=True)
